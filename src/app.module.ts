41cb31541a190dc1c7181f20d342037331e24bd5
Donald G <donald.g@me.com>
2022-10-17 19:12:27 -0400

    Add app db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MembersModule } from './members/members.module';
 import { GamesModule } from './games/games.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MembersModule, GamesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+    }),
+    MembersModule,
+    GamesModule,
+  ],
 })
 export class AppModule {}
