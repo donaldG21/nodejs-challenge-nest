0e363b3544a296b9949a0f4e53dbec6ddee8b20c
Donald G <donald.g@me.com>
2022-10-18 01:24:47 -0400

    Add find all to games service

--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Game } from './game.entity';
 
+/**
+ * Service dealing with game operations.
+ *
+ * @class
+ */
 @Injectable()
-export class GamesService {}
+export class GamesService {
+  /**
+   * Create an instance of class.
+   *
+   * @constructs
+   *
+   * @param {Repository<Game>} gameRepository
+   */
+  constructor(
+    @InjectRepository(Game)
+    private gameRepository: Repository<Game>,
+  ) {}
+
+  /**
+   * Fetch all games from db
+   * @returns {Promise}
+   */
+  findAll(): Promise<Game[]> {
+    return this.gameRepository.find();
+  }
+}
