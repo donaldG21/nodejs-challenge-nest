e09c60c27a24d1d7c8f8b0117a2b1af4e35515ed
Donald G <donald.g@me.com>
2022-10-18 01:35:45 -0400

    Make entities available to modules

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Game } from './game.entity';
 import { GamesController } from './games.controller';
 import { GamesService } from './games.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Game])],
   controllers: [GamesController],
   providers: [GamesService],
 })
