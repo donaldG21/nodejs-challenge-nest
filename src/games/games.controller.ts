23026eefc2d9ac4b93ff147240aa6bd20f27394f
Donald G <donald.g@me.com>
2022-10-19 22:09:18 -0400

    Move streaks endpoint to members controller

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateGameDto } from './dto/create-game.dto';
 import { Game } from './game.entity';
 import { GamesService } from './games.service';
@@ -23,18 +23,6 @@ export class GamesController {
   @Post()
   create(@Body() createGameDto: CreateGameDto): Promise<Game> {
     return this.gamesService.create(createGameDto);
-  }
-
-  /**
-   * Get streaks of days games played
-   * when more games were played than the day before
-   * e.g. Member played 2 games on 03/02, 3 games on 03/05,
-   * and 6 games on 03/06. That’s a streak.
-   * @returns {Date[][]}
-   */
-  @Get(':id/streaks')
-  getStreaks(@Param('id') id: number): Promise<Date[][]> {
-    return this.gamesService.getStreaks(id);
   }
 
   /**
--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { Member } from './member.entity';
 import { MembersService } from './members.service';
 
@@ -13,5 +13,17 @@ export class MembersController {
   @Get()
   findAll(): Promise<Member[]> {
     return this.membersService.findAll();
+  }
+
+  /**
+   * Get streaks of days games played
+   * when more games were played than the day before
+   * e.g. Member played 2 games on 03/02, 3 games on 03/05,
+   * and 6 games on 03/06. That’s a streak.
+   * @returns {Date[][]}
+   */
+  @Get(':id/streaks')
+  getStreaks(@Param('id') id: number): Promise<Date[][]> {
+    return this.membersService.getStreaks(id);
   }
 }
