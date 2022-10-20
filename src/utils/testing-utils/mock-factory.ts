16546cfb8253d9b31ccee8b505b05dfa78fac795
Donald G <donald.g@me.com>
2022-10-20 11:50:45 -0400

    Add missing jest mocks fn on entity

--- src/utils/testing-utils/mock-factory.ts
+++ src/utils/testing-utils/mock-factory.ts
@@ -7,6 +7,7 @@ export type MockType<T> = {
 export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(
   () => ({
     findOne: jest.fn((entity) => entity),
+    findOneBy: jest.fn((entity) => entity),
     find: jest.fn((entity) => entity),
     save: jest.fn((entity) => entity),
   }),
